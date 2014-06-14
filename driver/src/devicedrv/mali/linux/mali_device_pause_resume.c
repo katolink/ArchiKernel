7e27ca4aeb5136b5fed421c7797fd80ad1a0a4f7
JustArchi <JustArchi@JustArchi.net>
2014-06-14 04:00:45 +0200

    r3p2 rel3

--- driver/src/devicedrv/mali/linux/mali_device_pause_resume.c
+++ driver/src/devicedrv/mali/linux/mali_device_pause_resume.c
@@ -13,30 +13,23 @@
  * Implementation of the Mali pause/resume functionality
  */
 
+#include <linux/module.h>
+#include <linux/mali/mali_utgard.h>
 #include "mali_gp_scheduler.h"
 #include "mali_pp_scheduler.h"
-#include "mali_group.h"
 
-void mali_dev_pause(mali_bool *power_is_on)
+void mali_dev_pause(void)
 {
-	mali_bool power_is_on_tmp;
-
-	/* Locking the current power state - so it will not switch from being ON to OFF, but it might remain OFF */
-	power_is_on_tmp = _mali_osk_pm_dev_ref_add_no_power_on();
-	if (NULL != power_is_on)
-	{
-		*power_is_on = power_is_on_tmp;
-	}
-
 	mali_gp_scheduler_suspend();
 	mali_pp_scheduler_suspend();
 }
+
+EXPORT_SYMBOL(mali_dev_pause);
 
 void mali_dev_resume(void)
 {
 	mali_gp_scheduler_resume();
 	mali_pp_scheduler_resume();
-
-	/* Release our PM reference, as it is now safe to turn of the GPU again */
-	_mali_osk_pm_dev_ref_dec_no_power_on();
 }
+
+EXPORT_SYMBOL(mali_dev_resume);
