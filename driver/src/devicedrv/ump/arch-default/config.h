bf6958c573a482f1331172585fdbc1faf9fed7e8
JustArchi <JustArchi@JustArchi.net>
2014-06-14 04:02:10 +0200

    r4p0 rel1

--- driver/src/devicedrv/ump/arch-default/config.h
+++ driver/src/devicedrv/ump/arch-default/config.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2010, 2013-2014 ARM Limited. All rights reserved.
+ * Copyright (C) 2010, 2012, 2014 ARM Limited. All rights reserved.
  * 
  * This program is free software and is provided to you under the terms of the GNU General Public License version 2
  * as published by the Free Software Foundation, and any use by you of this program is subject to the terms of such GNU licence.
@@ -8,20 +8,17 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
-/**
- * @file mali_osk_math.c
- * Implementation of the OS abstraction layer for the kernel device driver
- */
+#ifndef __ARCH_CONFIG_H__
+#define __ARCH_CONFIG_H__
 
-#include "mali_osk.h"
-#include <linux/bitops.h>
+/* Use OS memory. */
+#define ARCH_UMP_BACKEND_DEFAULT          1
 
-u32 _mali_osk_clz(u32 input)
-{
-	return 32 - fls(input);
-}
+/* OS memory won't need a base address. */
+#define ARCH_UMP_MEMORY_ADDRESS_DEFAULT   0x00000000
 
-u32 _mali_osk_fls(u32 input)
-{
-	return fls(input);
-}
+/* 512 MB maximum limit for UMP allocations. */
+#define ARCH_UMP_MEMORY_SIZE_DEFAULT 512UL * 1024UL * 1024UL
+
+
+#endif /* __ARCH_CONFIG_H__ */
